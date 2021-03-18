99ddcbe4d7acc56332b42b84946fa7ed29c862de
Roy Shay <roy.shay@gmail.com>
2021-03-18 16:53:26 +0000

    Add search logic to macro

--- src/ModelSearchServiceProvider.php
+++ src/ModelSearchServiceProvider.php
@@ -2,12 +2,41 @@
 
 namespace Tooshay\ModelSearch;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Tooshay\ModelSearch\Commands\ModelSearchCommand;
 
 class ModelSearchServiceProvider extends PackageServiceProvider
 {
+    public function boot(): ModelSearchServiceProvider
+    {
+        Builder::macro('whereLike', function ($attributes, string $searchTerm) {
+            foreach (explode(' ', $searchTerm) as $searchTerm) {
+                $this->where(function (Builder $query) use ($attributes, $searchTerm) {
+                    foreach (Arr::wrap($attributes) as $attribute) {
+                        $query->when(
+                            str_contains($attribute, '.'),
+                            function (Builder $query) use ($attribute, $searchTerm) {
+                                [$relationName, $relationAttribute] = explode('.', $attribute);
+
+                                $query->orWhereHas($relationName, function (Builder $query) use ($relationAttribute, $searchTerm) {
+                                    $query->where($relationAttribute, 'LIKE', "%{$searchTerm}%");
+                                });
+                            },
+                            function (Builder $query) use ($attribute, $searchTerm) {
+                                $query->orWhere($attribute, 'LIKE', "%{$searchTerm}%");
+                            }
+                        );
+                    }
+                });
+            }
+
+            return $this;
+        });
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
